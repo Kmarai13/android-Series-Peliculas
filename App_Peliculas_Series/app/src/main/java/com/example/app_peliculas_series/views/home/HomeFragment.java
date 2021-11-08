14fd3830dc6c4bc0c03fafa5b23265c8bbdcb00f
Karla Marai Aguilar Chavez <kmarai13@live.com.mx>
2021-11-07 20:43:50 -0600

    UPDATE Se hace el llamado a los consumos de servicios
    Se consumen servicios de token para iniciar sesion y el usuario apruebe la sesion, y generar el token access, asi mismo se consume el servicio de obtener lista de peliculas

--- App_Peliculas_Series/app/src/main/java/com/example/app_peliculas_series/views/home/HomeFragment.java
+++ App_Peliculas_Series/app/src/main/java/com/example/app_peliculas_series/views/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.example.app_peliculas_series.views.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,12 +15,24 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.app_peliculas_series.R;
+import com.example.app_peliculas_series.adapters.PeliculasSeriesAdapter;
 import com.example.app_peliculas_series.databinding.FragmentHomeBinding;
+import com.example.app_peliculas_series.presenter.ViewFilmsPresenter;
+import com.example.app_peliculas_series.presenter.callbacks.AccessTokenListener;
+import com.example.app_peliculas_series.server.json.accesstoken.AccessTokentResponse;
+import com.example.app_peliculas_series.server.json.getlist.GetListResponse;
+import com.example.app_peliculas_series.server.json.requesttoken.RequestTokenResponse;
+import com.example.app_peliculas_series.utils.SingletonPrefs;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements AccessTokenListener, PeliculasSeriesAdapter.OnItemClickListener {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private ViewFilmsPresenter viewFilmsPresenter;
+    protected Button btnAcept, btnContinuar;
+    protected TextView textView;
+    private SingletonPrefs singletonPrefs;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,20 +41,84 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        getPresenter();
+        singletonPrefs = SingletonPrefs.getInstance();
+        initView();
 
-//        final TextView textView = binding.textHome;
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-//                textView.setText(s);
+                textView.setText(s);
             }
         });
         return root;
+    }
+
+    private void initView() {
+        textView = binding.tvList;
+        btnAcept = binding.btnAcept;
+        btnContinuar = binding.btnContinuar;
+
+        btnAcept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewFilmsPresenter.sendRequestToken();
+
+            }
+        });
+        btnContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewFilmsPresenter.sendTokenAcccess();
+            }
+        });
+
+    }
+
+    protected ViewFilmsPresenter getPresenter() {
+        return viewFilmsPresenter = new ViewFilmsPresenter(getContext(), this);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+
+    @Override
+    public void onSuccessAccessToken(AccessTokentResponse accessTokentResponse) {
+        singletonPrefs.accessTokentResponse = accessTokentResponse;
+        viewFilmsPresenter.sendGetListTo();
+    }
+
+    @Override
+    public void onSuccessRequestToken(RequestTokenResponse requestTokenResponse) {
+        singletonPrefs.requestTokenResponse = requestTokenResponse;
+        String url = "https://www.themoviedb.org/auth/access?request_token="+ requestTokenResponse.request_token;
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+
+    @Override
+    public void onSuccessGetList(GetListResponse requestTokenResponse) {
+        textView.setText(String.valueOf(requestTokenResponse.total_results));
+    }
+
+    @Override
+    public void onFailedService(String message) {
+
+    }
+
+    @Override
+    public void onShowAlert(String msg) {
+
+    }
+
+    @Override
+    public void onShowMessage(String msg) {
+
     }
 }
